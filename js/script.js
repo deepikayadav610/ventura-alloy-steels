f3dfec1757588f1aa5ea188b532691fe8f3ca9cd
DEEPIKA YADAV <138503237+deepikayadav610@users.noreply.github.com>
2025-06-21 13:55:57 +0530

    Ventura Dummy Website

--- js/script.js
+++ js/script.js
@@ -18,6 +18,13 @@ $(document).ready(function () {
             map.invalidateSize();  // Tells Leaflet to recalculate size
         }, 300);
     });
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.closest('.navbar').length) {
+            $(".navbar-toggler").click(); // Trigger the collapse
+        }
+    });
 });
 
 
